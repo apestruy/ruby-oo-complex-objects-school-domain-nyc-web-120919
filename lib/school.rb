972f5c0f6c43e5ad84391a20553662f2538ea093
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-10 22:08:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,8 +6,8 @@ class School
    attr_reader :roster
 
   def add_student(name, grade)
-    @roster = @roster[:grade]
-    @roster[:grade] << name 
+    @roster = @roster[grade]
+    @roster[grade] << name 
     @roster
   end 
   
