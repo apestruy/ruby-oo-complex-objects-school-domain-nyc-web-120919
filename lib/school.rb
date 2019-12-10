36160fc8e22311f20a81de08739f20f358748988
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-10 22:11:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ class School
   def add_student(name, grade)
     if @roster[grade]
     @roster[grade] << name
-  else @roster[grade][name]
+    else @roster[grade] = name 
+    end 
   end 
-end 
   
 end 
